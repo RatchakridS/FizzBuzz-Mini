0a85c6fab575bb094975978411af5d6a403dfc62
Ratchagrid <ratchakrid.s@arise.tech>
2024-05-15 10:55:41 +0700

    Implement TukTuk Function

--- testjs/src/taxi.test.ts
+++ testjs/src/taxi.test.ts
@@ -1,8 +1,59 @@
-import {Taxi} from './taxi'
+import {TukTuk} from './taxi'
 
 describe('Taxi', () => {
-  it('example test', () => {
-    const result = Taxi()
-    expect(result).toEqual(55)
+  it('No Waiting Time', () => {
+    const result = TukTuk(10.5, 0)
+    expect(result).toEqual(42)
   })
+
+  // it('Waiting Time', () => {
+  //   const result = TukTuk(11.9, 5.5)
+  //   expect(result).toEqual(54)
+  // })
+
+  // it('Drive only 0.1 Km', () => {
+  //   const result = TukTuk(0.1, 0)
+  //   expect(result).toEqual(4)
+  // })
+
+  // it('Drive for 0.9 Km with 0.1 Waiting Time', () => {
+  //   const result = TukTuk(0.9, 0.1)
+  //   expect(result).toEqual(5)
+  // })
+
+  // it('Drive into 0 Km with 0 Waiting Time', () => {
+  //   const result = TukTuk(0 ,0)
+  //   expect(result).toEqual(0)
+  // })
+
+  // it('Drive for -1 Km. with -1 Waiting Time', () => {
+  //   const result = TukTuk(-1, -1)
+  //   expect(result).toEqual(0)
+  // })
+
+  // it('Drive for -0.1 Km. with -0.1 Waiting Time', () => {
+  //   const result = TukTuk(-0.1, -0.1)
+  //   expect(result).toEqual(0)
+  // })
+
+  // it('Drive for -0.5 Km. With -0.5 Waiting Time', () => {
+  //   const result = TukTuk(-0.5, -0.5)
+  //   expect(result).toEqual(0)
+  // })
+
+  // it('Drive for -0.9 Km. with -0.9 Waiting Time', () => {
+  //   const result = TukTuk(-0.9, -0.9)
+  //   expect(result).toEqual(0)
+  // })
+
+  // it('Drive for 100 Km. with 100 Waiting Time', () => {
+  //   const result = TukTuk(100, 100)
+  //   expect(result).toEqual(500)
+  // })
+
+  // it('Drive for 10000000000000 Km with 100000000 Waiting Time', () => {
+  //   const result = TukTuk(10000000000000, 100000000)
+  //   let expected = (4 * 10000000000000 ) + (1 * 100000000)
+  //   expect(result).toEqual(expected)
+  // })
 })
--- testjs/src/taxi.ts
+++ testjs/src/taxi.ts
@@ -1,1 +1,23 @@
-export const Taxi = () => 55
+// export const Taxi = () => 55
+
+export function TukTuk(runningDistance: number, waitTime: number) {
+    runningDistance = RoundNumber(runningDistance)
+    waitTime = RoundNumber(waitTime)
+    let calculate = (4 * runningDistance) + (1 * waitTime)
+    return calculate
+}
+
+function RoundNumber(value: number) {
+    value = value < 0 ? 0 : value
+    const integerPart = Math.floor(value);
+    const decimalPart = value - integerPart;
+
+  if (decimalPart >= 0.1 && decimalPart <= 0.4) {
+    return integerPart + 0.5;
+  } else if (decimalPart >= 0.6 && decimalPart <= 0.9) {
+    return integerPart + 1;
+  } else {
+    // For other cases, return the number itself or handle differently if needed
+    return Math.round(value);
+  }
+}
