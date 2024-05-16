a6b9ccef8a7181b98d13965e238aca71952969b4
Ratchagrid <ratchakrid.s@arise.tech>
2024-05-16 09:25:46 +0700

    TestFizzBuzzShouldReturn1WhenInput8

--- testgo/fizzbuzz_test.go
+++ testgo/fizzbuzz_test.go
@@ -82,3 +82,15 @@ func TestFizzBuzzShouldReturn1WhenInput7(t *testing.T) {
 
 	}
 }
+
+func TestFizzBuzzShouldReturn1WhenInput8(t *testing.T) {
+	input := 8
+
+	got := FizzBuzz(input)
+
+	want := "8"
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+
+	}
+}
