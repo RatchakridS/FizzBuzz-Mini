dc9f82975e567767bbab480f10df5c510bc92e6b
Ratchagrid <ratchakrid.s@arise.tech>
2024-05-17 09:26:00 +0700

    remove all test case and code

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -1,14 +1,5 @@
 package main
 
-import "strconv"
-
 func FizzBuzz(n int) string {
-	if n%15 == 0 {
-		return "FizzBuzz"
-	} else if n%5 == 0 {
-		return "Buzz"
-	} else if n%3 == 0 {
-		return "Fizz"
-	}
-	return strconv.Itoa(n)
+
 }
