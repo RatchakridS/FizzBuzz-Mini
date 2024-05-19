c8cfe00197acf57fc40edb52e2dd5afc7218f106
Ratchagrid <ratchakrid.s@arise.tech>
2024-05-19 13:16:44 +0700

    remove all code

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -1,22 +1,6 @@
 package main
 
-import "strconv"
-
 func FizzBuzz(n int) string {
 
-	result := ""
-
-	fizz := map[bool]string{true: "Fizz", false: ""}
-	buzz := map[bool]string{true: "Buzz", false: ""}
-
-	fizzPart := fizz[n%3 == 0]
-	buzzPart := buzz[n%5 == 0]
-
-	result = fizzPart + buzzPart
-
-	if result == "" {
-		result = strconv.Itoa(n)
-	}
-
-	return result
+	return "1"
 }
