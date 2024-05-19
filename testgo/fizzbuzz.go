0d73d344e43a9b6d640eb3aa38bd68ddd4c1194a
Ratchagrid <ratchakrid.s@arise.tech>
2024-05-19 15:23:43 +0700

    TestFizzBuzzShouldReturnBuzzWhenInput10

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -3,15 +3,16 @@ package main
 func FizzBuzz(n int) string {
 
 	fizzBuzzConvention := map[int]string{
-		1: "1",
-		2: "2",
-		3: "Fizz",
-		4: "4",
-		5: "Buzz",
-		6: "Fizz",
-		7: "7",
-		8: "8",
-		9: "Fizz",
+		1:  "1",
+		2:  "2",
+		3:  "Fizz",
+		4:  "4",
+		5:  "Buzz",
+		6:  "Fizz",
+		7:  "7",
+		8:  "8",
+		9:  "Fizz",
+		10: "Buzz",
 	}
 
 	return fizzBuzzConvention[n]
--- testgo/fizzbuzz_test.go
+++ testgo/fizzbuzz_test.go
@@ -100,3 +100,14 @@ func TestFizzBuzzShouldReturn9WhenInput9(t *testing.T) {
 		t.Errorf("got %q but want %q", got, want)
 	}
 }
+
+func TestFizzBuzzShouldReturnBuzzWhenInput10(t *testing.T) {
+	input := 10
+
+	got := FizzBuzz(input)
+
+	want := "Buzz"
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+	}
+}
