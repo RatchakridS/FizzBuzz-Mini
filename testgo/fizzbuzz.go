cdaf1d62fc307d5f61dc25d7cdb1d9d23ce27a4c
Ratchagrid <ratchakrid.s@arise.tech>
2024-05-19 16:58:38 +0700

    TestLv3/lv:ShouldReturn2WhenInput2

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -1,10 +1,12 @@
 package main
 
+import "strconv"
+
 var fizz = "Fizz"
 var buzz = "Buzz"
 
 // ----------------- Lv3. -----------------
 
 func FizzBuzzLv3(n int) string {
-	return "1"
+	return strconv.Itoa(n)
 }
