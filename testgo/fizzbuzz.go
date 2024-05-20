358a407cd243bea24ee8e61e3d9b8e75e7b7de7b
Ratchagrid <ratchakrid.s@arise.tech>
2024-05-20 09:25:19 +0700

    lv3:ShouldReturn1WhenInput1

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -1,9 +1,9 @@
 package main
 
-import "strconv"
-
 // ----------------- Lv3. -----------------
 
 func FizzBuzzLv3(n int) string {
-	return strconv.Itoa(n)
+	convention := map[int]string{}
+	convention[1] = "1"
+	return convention[n]
 }
